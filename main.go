51e0e950df94745cb6ab72eb88bdb1242997b238
9ziggy9 <davidarogers@protonmail.com>
2023-02-24 11:27:16 -0800

    gorilla imported basic server running

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
   "fmt"
+  "log"
+  "github.com/gorilla/mux"
+  "net/http"
 )
 
 func main() {
-  fmt.Println("Hello, world!")
+  r := mux.NewRouter()
+
+  // ROUTE WITH ANON HANDLER
+  r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, World!")
+  }).Methods("GET")
+
+  fmt.Println("Starting server on :8000 ...")
+  err := http.ListenAndServe(":8000", r)
+  if err != nil {
+    defer func() {
+      log.Println("Hit deferred function, panicked out")
+      log.Fatal(err)
+    }()
+    panic(err)
+  }
 }
